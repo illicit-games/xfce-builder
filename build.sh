c8678bb23e18df41b98b89ede221d94356c1bd90
illicit-games <156360517+illicit-games@users.noreply.github.com>
2024-01-22 00:39:12 -0500

    pcmanfm, using htop, powertop vs xfce4-taskmngr, and profiler
    This will look nicer on my duel monitor setup, also the last build i ran into an issue it wasn't using any filesystem so this latest script uses pcmanfm vs thunar. 
    
    I also plan on creating a hook to fix the large overhead of thumblerd or xfce4's tumbnail generator tool as its not 100% needed for everything. At least not in my book.

--- build.sh
+++ build.sh
@@ -15,15 +15,15 @@ cd $BUILD
 
 # Live Build Configuration
 
-lb config --binary-images iso --mode debian --architectures amd64 --linux-flavours amd64 --distribution trixie --archive-areas "main non-free-firmware" --memtest none --updates true --security true --cache true --apt-recommends true --apt-source-archives false --source false --firmware-binary true --firmware-chroot true --win32-loader false --iso-application $BUILD --iso-preparer illicitgames-https://github.com/illicit-games/xfce-builder/ --iso-publisher illicitgames-https://github.com/illicit-games/xfce-builder/ --image-name "$BUILD-$(date -u +"%y%m%d")" --iso-volume "$BUILD-$(date -u +"%y%m%d")" --checksums sha512 --clean --color
+lb config --binary-images iso-hybrid --mode debian --architectures amd64 --linux-flavours amd64 --distribution trixie --archive-areas "main non-free-firmware" --memtest none --updates true --security true --cache true --apt-recommends true --apt-source-archives false --source false --firmware-binary true --firmware-chroot true --win32-loader false --iso-application $BUILD --iso-preparer illicitgames-https://github.com/illicit-games/xfce-builder/ --iso-publisher illicitgames-https://github.com/illicit-games/xfce-builder/ --image-name "$BUILD-$(date -u +"%y%m%d")" --iso-volume "$BUILD-$(date -u +"%y%m%d")" --checksums sha512 --clean --color
 
 # Install Desktop
 
-echo "xfce4-appfinder xfce4-panel xfce4-session xfce4-settings xfce4-terminal xfconf xfdesktop4 xfwm4 xfce4-whiskermenu-plugin xfce4-power-manager xfce4-power-manager-plugins xfce4-taskmanager xfce4-systemload-plugin xfce4-screenshooter xdm network-manager-gnome network-manager-config-connectivity-debian macchanger pwgen firefox-esr weechat xarchiver p7zip-full gparted" > $WORKING/$BUILD/config/package-lists/desktop.list.chroot
+echo "xfce4-appfinder xfce4-panel xfce4-session xfce4-settings xfce4-terminal pcmanfm xfconf xfdesktop4 xfwm4 xfce4-whiskermenu-plugin xfce4-power-manager xfce4-power-manager-plugins  xfce4-screenshooter network-manager-gnome network-manager-config-connectivity-debian macchanger pwgen firefox-esr weechat xarchiver p7zip-full gparted" > $WORKING/$BUILD/config/package-lists/desktop.list.chroot
 
 # Install Applications, all of these may be able to be removed using suggsted and recommends. we will see.
 
-echo "apt-transport-https autoconf automake build-essential cifs-utils dbus-user-session dbus-x11 debconf debhelper dh-autoreconf dialog dirmngr dkms dosfstools exfatprogs fakeroot ghostscript gir1.2-ibus-1.0 grub-pc hardinfo haveged ibus ibus-data ibus-gtk ibus-gtk3 iftop im-config iw jfsutils libibus-1.0-5 libxcb-xtest0 linux-headers-amd64 lsb-release lshw menu netcat-openbsd ntfs-3g os-prober pciutils perl policykit-1 squashfs-tools sudo syslinux syslinux-common udisks2 upower x11-common xauth xdg-utils" > $WORKING/$BUILD/config/package-lists/extrapackages.list.chroot
+echo "xdm htop powertop apt-transport-https autoconf automake build-essential cifs-utils dbus-user-session dbus-x11 debconf debhelper dh-autoreconf dialog dirmngr dkms dosfstools exfatprogs fakeroot ghostscript gir1.2-ibus-1.0 grub-pc hardinfo haveged ibus ibus-data ibus-gtk ibus-gtk3 iftop im-config iw jfsutils libibus-1.0-5 libxcb-xtest0 linux-headers-amd64 lsb-release lshw menu netcat-openbsd ntfs-3g os-prober pciutils perl policykit-1 squashfs-tools sudo syslinux syslinux-common udisks2 upower x11-common xauth xdg-utils" > $WORKING/$BUILD/config/package-lists/extrapackages.list.chroot
 
 
 # Install Firmware
